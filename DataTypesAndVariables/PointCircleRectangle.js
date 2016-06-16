853b642a79af93f7ed91d417849eac1ea13d1dc5
iliangogov <iliangogov@gmail.com>
2016-06-16 16:30:11 +0300

    homework

--- DataTypesAndVariables/PointCircleRectangle.js
+++ DataTypesAndVariables/PointCircleRectangle.js
@@ -1,3 +1,47 @@
 /**
  * Created by Gogov on 6/16/2016.
  */
+function solve(args) {
+    var X = Number(args[0]);
+    var Y = Number(args[1]);
+
+    var inCircle = false;
+    var inRectangle = false;
+    var defaultCircleX=1;
+    var defaultCircleY=1;
+    var distanceToCircleX = X - defaultCircleX;
+    var distanceToCircleY = Y - defaultCircleY;
+
+    var distanceToCircle = Math.sqrt(
+        (distanceToCircleX
+        * distanceToCircleX)
+        + (distanceToCircleY
+        * distanceToCircleY));
+
+    
+    if (distanceToCircle <= 1.5) {
+        inCircle = true;
+    }
+   
+    if (X >= -1 && X <= 5 && Y <= 1 && Y >= -1) {
+
+        inRectangle = true;
+    }
+
+    var output = '';
+    if (inCircle) {
+        output += 'inside circle ';
+    } else {
+        output += 'outside circle ';
+    }
+
+    if (inRectangle) {
+        output += 'inside rectangle';
+    } else {
+        output += 'outside rectangle';
+    }
+
+    console.log(output);
+}
+
+//solve(['2.5', '1']);
